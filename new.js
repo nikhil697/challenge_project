30c0e0e1b646304edbfdc5da93177aadb232d27e
nikhil697 <nikhilchadha1537@gmail.com>
2022-03-03 23:16:25 +0530

    Final Challenge project

--- new.js
+++ new.js
@@ -26,25 +26,39 @@ function hello(){
     var image4=document.getElementById("p4");
     if(a<1){
         s1+=1000;
-        s2+=800;
-        s3+=0;
+        // s2+=800;
+        
         s4 += 500;
         image1.setAttribute("src","./raja1.jpeg");
         image3.setAttribute("src","./Chor 1.jpg"); 
         image4.setAttribute("src","./SIPAHI1.jpg"); 
+        image3.addEventListener("click",function(){
+            s2+=800;
+            s3+=0;
+            //s3+=800;
+        });
+        s2-=800;
+        s3+=800;
         document.getElementById("s1").innerHTML=s1
         document.getElementById("s2").innerHTML=s2
         document.getElementById("s3").innerHTML=s3
         document.getElementById("s4").innerHTML=s4
     }
     else if(a>2){
-        s1+=0;
-        s2+=800;
+        // s1+=0;
+        // s2+=800;
         s3+=500;
         s4+=1000;
         image4.setAttribute("src","./raja1.jpeg");
         image1.setAttribute("src","./Chor 1.jpg"); 
         image3.setAttribute("src","./SIPAHI1.jpg");
+        image1.addEventListener("click",function(){
+            s2+=800;
+            s1+=0;
+            //s1+=800;
+        });
+        s2-=800;
+        s1+=800;
         document.getElementById("s1").innerHTML=s1
         document.getElementById("s2").innerHTML=s2
         document.getElementById("s3").innerHTML=s3
@@ -52,12 +66,19 @@ function hello(){
     }
     else{
         s1+=500;
-        s2+=800;
+        // s2+=800;
         s3+=1000;
-        s4+=0;
+        //s4+=0;
         image3.setAttribute("src","./raja1.jpeg");
         image4.setAttribute("src","./Chor 1.jpg"); 
         image1.setAttribute("src","./SIPAHI1.jpg");
+        image4.addEventListener("click",function(){
+            s2+=800;
+            s4+=0;
+            //s4+=800;
+        });
+        s2-=800;
+        s4+=800;
         document.getElementById("s1").innerHTML=s1
         document.getElementById("s2").innerHTML=s2
         document.getElementById("s3").innerHTML=s3
